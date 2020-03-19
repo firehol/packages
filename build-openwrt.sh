9c70c47b0c2bfdddb9e43b3617866b2238c520fe
Ravneet Singh <thecheatah@gmail.com>
2020-03-19 11:46:10 -0400

    Support OpenWRT 19.07

--- build-openwrt.sh
+++ build-openwrt.sh
@@ -14,7 +14,7 @@ IPRANGE_MD5=`cut -f1 -d' ' < build/iprange.md5`
 # it into a package, whereas iprange is a binary and must be compiled
 # for each target.
 
-osver=18.06
+osver=19.07
 mkdir -p build/openwrt-$osver
 cd build/openwrt-$osver
 rm -f build-list
@@ -43,9 +43,9 @@ do
   sed -i -e "s;<<VER>>;$IPRANGE_VERSION;" -e "s;<<URL>>;$IPRANGE_URL;" -e "s;<<MD5>>;$IPRANGE_MD5;" "$short"/package/iprange/Makefile
   echo "$short" >> build-list
 done <<!
-ar71xx_generic https://downloads.openwrt.org/releases/18.06.0/targets/ar71xx/generic openwrt-sdk-18.06.0-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64
-brcm47xx_generic https://downloads.openwrt.org/releases/18.06.0/targets/brcm47xx/generic openwrt-sdk-18.06.0-brcm47xx-generic_gcc-7.3.0_musl.Linux-x86_64
-ipq806x_generic https://downloads.openwrt.org/releases/18.06.0/targets/ipq806x/generic openwrt-sdk-18.06.0-ipq806x_gcc-7.3.0_musl_eabi.Linux-x86_64
+ar71xx_generic https://downloads.openwrt.org/releases/19.07.2/targets/ar71xx/generic openwrt-sdk-19.07.2-ar71xx-generic_gcc-7.5.0_musl.Linux-x86_64
+brcm47xx_generic https://downloads.openwrt.org/releases/19.07.2/targets/brcm47xx/generic openwrt-sdk-19.07.2-brcm47xx-generic_gcc-7.5.0_musl.Linux-x86_64
+ipq806x_generic https://downloads.openwrt.org/releases/19.07.2/targets/ipq806x/generic openwrt-sdk-19.07.2-ipq806x-generic_gcc-7.5.0_musl_eabi.Linux-x86_64
 !
 
 for t in `cat build-list`
