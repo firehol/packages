5e3cf4b260d2adfc70db28d840b2db8ce6481f15
Kyle Maas <kylemaasdev@gmail.com>
2022-09-19 09:20:10 -0400

    Add build support for EL8 and EL9

--- redhat/docker-setup.sh
+++ redhat/docker-setup.sh
@@ -6,4 +6,10 @@ set -e
 yum install -y rpm-build make gcc automake autoconf
 
 # FireHOL dependencies
-yum install -y iproute ipset iptables iptables-ipv6 tcpdump systemd zlib-devel libuuid-devel
+MAJOR_VERSION=`cat /etc/redhat-release | grep -o -P '[0-9]+\.' | head -n 1 | grep -o -P '[0-9]+'`
+if [ "${MAJOR_VERSION}" == "7" ]
+then
+	yum install -y iproute ipset iptables iptables-ipv6 tcpdump systemd zlib-devel libuuid-devel
+else
+	yum install -y iproute ipset iptables-services kmod tcpdump systemd zlib-devel libuuid-devel procps-ng iproute-tc
+fi
